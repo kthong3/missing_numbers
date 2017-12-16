b11ddad277142abfb836fcae5b0124256ac6319f
kthong3 <kthongsakounh@gmail.com>
2017-12-16 00:19:44 -0600

    create rspec tests

---
+++ missing_numbers_spec.rb
@@ -0,0 +1,20 @@
+require_relative 'missing_numbers'
+
+describe 'find_missing_num' do
+  before :each do
+    input_array = [4,7,9,3,5,8,2]
+  end
+
+  it 'returns an array' do
+    expect(find_missing_num(input_array)).to be_an Array
+  end
+
+  it 'returns an array excluding the numbers in input array' do
+    expect(find_missing_num(input_array)).not_to include input_array
+  end
+
+  it 'returns the missing numbers from array' do
+    expect(find_missing_num(input_array).to equal [1,6]
+  end
+
+end
