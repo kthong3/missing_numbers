8da65e7fa4f7f74d91d1d6817caba201e7ea8f37
kthong3 <kthongsakounh@gmail.com>
2017-12-16 00:52:25 -0600

    take out before each block and use let instead, passed rspec tests

--- missing_numbers_spec.rb
+++ missing_numbers_spec.rb
@@ -1,9 +1,7 @@
 require_relative 'missing_numbers'
 
 describe 'find_missing_num' do
-  before :each do
-    input_array = [4,7,9,3,5,8,2]
-  end
+  let(:input_array) { [4,7,9,3,5,8,2] }
 
   it 'returns an array' do
     expect(find_missing_num(input_array)).to be_an Array
@@ -14,7 +12,7 @@ describe 'find_missing_num' do
   end
 
   it 'returns the missing numbers from array' do
-    expect(find_missing_num(input_array).to equal [1,6]
+    expect(find_missing_num(input_array)).to eq [1,6]
   end
 
 end
