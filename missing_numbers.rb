2a52d1bd16133fe3b717455661f84a60ebf141c2
kthong3 <kthongsakounh@gmail.com>
2017-12-16 00:26:48 -0600

    pseudocode

---
+++ missing_numbers.rb
@@ -0,0 +1,12 @@
+# input: array of numbers, some may have repeats
+# output: array of missing numbers from input array
+
+# sort the input array of numbers
+# delete any repeated numbers
+# find largest number as it will be length of array
+# create new empty array for output
+# create counter starting at 1
+# while counter is less than length of array
+# add counter to output array if that counter is not included in the input array
+# increment counter so it would continue until the end of input array
+# print output array
